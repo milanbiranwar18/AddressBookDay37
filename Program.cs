9921e128502844158b3b576117a129c8dbf337db
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-09 16:54:39 +0530

    Adding program for new Contact to the address book

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBookDay37;
+
 Console.WriteLine("Wellcome to the address book problem");
+AddressBook addressbook1 = new AddressBook();
+addressbook1.AddToDataTable();
